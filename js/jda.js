3c36dc8665ac79f3d5889ca1cf3e83a6e5ea3660
John D. Ament <john.d.ament@gmail.com>
2013-05-12 20:23:10 -0400

    Started working on site.

--- js/jda.js
+++ js/jda.js
@@ -2,10 +2,10 @@ requirejs.config({
     "baseUrl": "js/site",
     "paths": {
       "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-	  "text":"//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.js",
-	  "backbone":"//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min.js",
-	  "underscore":"//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js",
-	  "bootstrap":"//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap.min.js"
+	  "text":"//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text",
+	  "backbone":"//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min",
+	  "underscore":"//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min",
+	  "bootstrap":"//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.1/js/bootstrap"
     },
 	"shim": {
 		'underscore': {
