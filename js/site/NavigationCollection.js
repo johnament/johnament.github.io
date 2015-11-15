2ae1c5dae52d7ea7ce831df49aeb6279413be210
John D. Ament <johndament@apache.org>
2015-11-15 09:44:27 -0500

    Removed old content

--- js/site/NavigationCollection.js
+++
@@ -1,9 +0,0 @@
-define(['underscore','backbone','NavigationModel'
-], function(_, Backbone, NavigationModel){
-  var NavigationCollection = Backbone.Collection.extend({
-    model: NavigationModel,
-	url: 'content/navigation.json'
-  });
-  // You don't usually return a collection instantiated
-  return NavigationCollection;
-});
--- js/site/init.js
+++
@@ -1,6 +0,0 @@
-define(['jquery','backbone','Workspace'], function($,Backbone,Workspace) {
-	$(function(){
-		new Workspace();
-		Backbone.history.start({pushState: true});
-	});
-});
